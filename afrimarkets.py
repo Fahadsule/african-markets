9de32735a5461cd7d692c2f347279e67cdc64e42
Fahadsule <fahadsuleiman31@gmail.com>
2026-02-15 03:30:43 +0300

    just finished more functions

--- afrimarkets.py
+++ afrimarkets.py
@@ -2,7 +2,7 @@ import pandas as pd
 import psycopg2
 import sqlalchemy
 import numpy as np
-import statsmodels
+import statsmodels.api as sm
 
 db_connection_string = "postgresql://fahad:589Aupgradez2BdfK@localhost:5432/africanfinance_db"
 engine = sqlalchemy.create_engine(db_connection_string)
@@ -79,6 +79,7 @@ def get_daily_price(exchange, symbol, start_date, end_date):
         return df
     else:
         print("WE CURRENTLY LACK DATA ON THAT EXCHANGE....")
+        return None
 
 def get_no_dividend_daily_return(exchange, symbol, start_date, end_date,ignore_zero_volume):
     df=get_daily_price(exchange,symbol,start_date,end_date)
@@ -87,6 +88,15 @@ def get_no_dividend_daily_return(exchange, symbol, start_date, end_date,ignore_z
         df=df[df['volume']>0]
     df['daily_return']=df['closing_price'].pct_change()
     df=df[['trade_date','ticker','company_name','daily_return']]
+    return df
+
+def get_log_daily_return(exchange, symbol,start_date,end_date,ignore_zero_volume):
+    df=get_daily_price(exchange,symbol,start_date,end_date)
+    df['volume']=pd.to_numeric(df['volume'],errors='coerce')
+    if ignore_zero_volume==True:
+        df=df[df['volume']>0]
+    df['log_return']=np.log(df['closing_price']/df['closing_price'].shift(1))
+    df=df[['trade_date','ticker','company_name','log_return']]
     return df
 
 
@@ -146,6 +156,13 @@ def get_index_return(exchange,start_date,end_date):
     df['daily_return']=df['closing_price'].pct_change()
     df=df[['trade_date','ticker','daily_return']]
     return df
+
+def get_log_index_return(exchange,start_date,end_date):
+    df=get_index_price(exchange,start_date,end_date)
+    df['log_return']=np.log(df['closing_price']).diff()
+    df=df[['trade_date','ticker','log_return']]
+    return df
+
 
 def get_dividend_data(exchange,symbol,start_date,end_date):
     
@@ -207,6 +224,7 @@ def get_bonus_issue_data(exchange,symbol,start_date,end_date):
         return df
     else:
         print("WE CURRENTLY LACK DISTRIBUTION DATA ON THIS EXCHANGE")
+        return None
     
 def get_splits_data(exchange,symbol,start_date,end_date):
     if exchange in splits_available:
@@ -225,6 +243,7 @@ def get_splits_data(exchange,symbol,start_date,end_date):
         return df
     else:
         print("WE CURRENTLY LACK STOCK SPLITS DATA ON THIS EXCHANGE")
+        return None
 
 
 def get_rights_data(exchange,symbol,start_date,end_date):
@@ -244,6 +263,7 @@ def get_rights_data(exchange,symbol,start_date,end_date):
         return df
     else:
         print("WE CURRENTLY LACK RIGHTS DATA ON THIS EXCHANGE..........")
+        return None
 
 def get_ticker_changes(exchange):
     if exchange in ticker_changes:
@@ -254,4 +274,88 @@ def get_ticker_changes(exchange):
         return df
     else:
         print("NO TICKER CHANGE RECORDED FOR THIS EXCHANGE.................")
+        return None
 
+def get_ticker_list(exchange):
+    if exchange in exchanges_available:
+        table=Exchange_tables[exchange]
+        sql=f"SELECT DISTINCT ticker FROM {table}"
+        df=pd.read_sql_query(sql,engine)
+        change_df=get_ticker_changes(exchange)
+        change_list=change_df['old_ticker'].to_list()
+        ticker_list=df['ticker'].to_list()
+        ticker_list = list(set(ticker_list) - set(change_list))
+        ticker_list.sort()
+        return ticker_list
+    else:
+        print("WE LACK ANY DATA ON THIS EXCHANGE")
+        return None
+
+def get_company_list(exchange):
+    if exchange in exchanges_available:
+        table=Exchange_tables[exchange]
+        sql=f"SELECT DISTINCT company_name FROM {table}"
+        df=pd.read_sql_query(sql,engine)
+        company_list=df['company_name'].to_list()
+        return company_list
+    else:
+        print("WE LACK ANY DATA ON THIS EXCHANGE")
+        return None
+
+def get_industry_list(exchange):
+    if exchange in exchanges_available:
+        table=Exchange_tables[exchange]
+        sql=f"SELECT DISTINCT industry FROM {table}"
+        df=pd.read_sql_query(sql,engine)
+        if 'industry' in df.columns:
+            industry_list=df['industry'].to_list()
+            return industry_list
+        else:
+            print("WE ARE STILL WORKING ON GETTING INDUSTRY CATEGORIES FOR THIS EXCHANGE............")
+    else:
+        print("WE LACK ANY DATA ON THIS EXCHANGE")
+        return None
+
+def get_volatility(exchange,symbol):
+    if exchange in exchanges_available:
+        df=get_no_dividend_daily_return(exchange,symbol,start_date='all',end_date='max',ignore_zero_volume=True)
+        #filter for the last 242 days
+        df=df.tail(242)
+        volatility=df['daily_return'].std()
+        yearly_volatility=volatility*np.sqrt(242)
+        data={'symbol':symbol,
+              'daily_volatility':volatility,
+              'annualized_volatility':yearly_volatility}
+        vol_df=pd.DataFrame([data])
+        return vol_df
+    else:
+        print("WE CURRENTLY LACK DATA ON THIS EXCHANGE..................")
+        return None
+
+def get_unlevered_beta(exchange,symbol):
+    if exchange in exchanges_available:
+        ticker_df=get_no_dividend_daily_return(exchange,symbol,start_date='all',end_date='max',ignore_zero_volume=True)
+        ticker_df=ticker_df.tail(242)
+        ticker_df=ticker_df[['trade_date','daily_return']]
+        index_df=get_index_return( exchange,start_date='all',end_date='max')
+        index_df=index_df[['trade_date','daily_return']]
+        df=ticker_df.merge(index_df,on='trade_date',how='inner',suffixes=("_stock","_index"))
+        X=sm.add_constant(df['daily_return_index'])
+        y=df['daily_return_stock']
+        model=sm.OLS(y,X).fit()
+        beta=model.params["daily_return_index"]
+        alpha=model.params["const"]
+        r2=model.rsquared
+        t_beta=model.tvalues["daily_return_index"]
+        p_beta=model.pvalues["daily_return_index"]
+        data={"ticker":symbol,
+              "unleverd_beta":beta,
+              "alpha":alpha,
+              "R2":r2,
+              "T Value":t_beta,
+              "P Value":p_beta}
+        beta_df=pd.DataFrame([data])
+        return beta_df
+    else:
+        print('WE CURRRENTLY LACK DATA ON THIS EXCHANGE')
+        return None
--- test.py
+++ test.py
@@ -1,12 +1,5 @@
 import afrimarkets as af
+import pandas as pd
 
-t1=af.get_daily_price("NAE",'CG','all','max')
-
-
-
-t2=af.get_index_price
-
-t3=af.get_rights_data("DSE","TCL","all","max")
-
-t4=af.get_ticker_changes("NASE")
-print(t4)
+t=af.get_unlevered_beta("NASE","CGEN")
+print(t)
